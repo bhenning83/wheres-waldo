db7176bb58beb4881279235ffcbba73cfc261c51
Brendon Henning <brendonhenning@gmail.com>
2021-04-19 13:23:20 -0500

    init commit

---
+++ app/javascript/packs/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
