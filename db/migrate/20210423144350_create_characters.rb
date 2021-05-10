0aaf672f17a49d4be30af4a00c2b821ea93d984c
Brendon Henning <brendonhenning@gmail.com>
2021-05-09 22:24:38 -0500

    changed strings to text

--- db/migrate/20210423144350_create_characters.rb
+++ db/migrate/20210423144350_create_characters.rb
@@ -1,7 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     create_table :characters do |t|
-      t.string :name
+      t.text :name
 
       t.timestamps
     end
--- db/migrate/20210423144431_create_coordinates.rb
+++ db/migrate/20210423144431_create_coordinates.rb
@@ -2,7 +2,7 @@ class CreateCoordinates < ActiveRecord::Migration[6.1]
   def change
     create_table :coordinates do |t|
       t.belongs_to :character
-      t.string :location
+      t.text :location
 
       t.timestamps
     end
